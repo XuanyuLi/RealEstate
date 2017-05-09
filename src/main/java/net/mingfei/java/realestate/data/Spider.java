8e8e950f8d7beaafe52d0254db5cb828b3665f30
mingfei <mingfei.net@gmail.com>
2017-05-09 14:16:29 +0800

    Update

--- src/main/java/net/mingfei/java/realestate/data/Spider.java
+++ src/main/java/net/mingfei/java/realestate/data/Spider.java
@@ -2,6 +2,7 @@ package net.mingfei.java.realestate.data;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -16,6 +17,9 @@ public class Spider {
         String url = "http://bj.lianjia.com/ershoufang/dongcheng/";
         Document document = Jsoup.connect(url).get();
         Elements elements = document.select("li[class=clear]");
-        System.out.println(elements.size());
+        for (Element element : elements) {
+            String region = element.select("a[data-el=region]").get(0).text();
+            System.out.println(region);
+        }
     }
 }
